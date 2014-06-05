abe21de6d0de0750919d076d802e9a22e8bc6d10
Gwanwoo <rkdrhksdn2@nate.com>
2014-06-05 17:01:43 +0900

    commit
    dfd

--- src/app.js
+++ src/app.js
@@ -1,3 +1,9 @@
 /**
  * 
- */
+ */function autoSave(event){
+	 val value = $('#contents').val();
+ 
+	 window.localStorage.setItem('note_contents',value);
+ }
+ 
+ $('#contents').on('keyup',autoSave);
