ec337af9af3e9ca2d6ea60b7bdbf2023fca6d3ab
Gwanwoo <rkdrhksdn2@nate.com>
2014-06-05 17:09:55 +0900

    commit
    gogo

--- src/app.js
+++ src/app.js
@@ -1,3 +1,13 @@
 /**
  * 
- */
+ */function autoSave(event){
+	 val value = $('#contents').val();
+ 
+	 window.localStorage.setItem('note_contents',value);
+ }
+ 
+ function autoLoad(event){
+	 $('#contents').val(window.localStorage.getItem('note_contents'));
+ }
+ 
+ $('#contents').on('keyup',autoSave);
